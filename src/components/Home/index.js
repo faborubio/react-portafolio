add3d90b6648a53482d250b07b1073c715f6c7c8
Fabian Rubio <fabian.rubiocs@gmail.com>
2023-03-23 22:31:59 -0300

    add somes style

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,37 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
-import LogoF from '../../assets/images/logof2.png'
+import LogoTitle from '../../assets/images/logo-s.png'
+import AnimatedLetters from "../AnimatedLetters"
 import './index.scss'
 
 const Home = () => {
+  const[letterClass, setLetterClass] = useState("text-animate")
+  const nameArray = ["a","b","i","a","n"]
+  const jobArray = ["w","e","b"," ", "d","e","v","e","l","o","p","e","r", "."]
+
+  useEffect(() => {
+    return setTimeout(() => {
+        setLetterClass("text-animate-hover")
+    }, 4000);
+  }, [])
 
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi, <br /> I'am
-        <img src={LogoF} alt="developer"/>
-        Fabian
+        <h1>
+        <span className={letterClass}>H</span>
+        <span className={`${letterClass} _12`}>i,</span>
         <br />
-        web developer
+        <span className={`${letterClass} _13`}>I</span>
+        <span className={`${letterClass} _14`}>'m</span>
+        <img src={LogoTitle} alt="developer"/>
+        <AnimatedLetters letterClass={letterClass}
+        strArray={nameArray}
+        idx={15}/>
+        <br />
+        <AnimatedLetters letterClass={letterClass}
+        strArray={jobArray}
+        idx={20}/>
         </h1>
         <h2> Fullstack Developer who love to learn</h2>
         <Link to="/contact" className="flat-button">Contact me</Link>
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -6,16 +6,16 @@ const Layout = () => {
   return (
   <div className="App">
     <Sidebar />
-    <div className="page">
-        <span className="tags top-tags">&lt;body&gt;</span>
-        <Outlet/>
+      <div className="page">
+          <span className="tags top-tags">&lt;body&gt;</span>
+          <Outlet/>
 
-        <span className="tags bottom-tags">
-            &lt;/body&gt;
-            <br/>
-            <span className="bottom-tag-html">&lt;/html&gt;</span>
-        </span>
-    </div>
+          <span className="tags bottom-tags">
+              &lt;/body&gt;
+              <br/>
+              <span className="bottom-tag-html">&lt;/html&gt;</span>
+          </span>
+      </div>
   </div>
 
   )
